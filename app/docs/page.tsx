8731fd25b2c21ee7edff977b75776272d0268f86
noisk8 <juansk8jaramillo@gmail.com>
2023-12-10 05:46:01 -0500

    porfa2

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,9 +1,11 @@
 import { title } from "@/components/primitives";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function DocsPage() {
 	return (
 		<div>
 			<h1 className={title()}>Docs</h1>
+			<SpeedInsights/>
 		</div>
 	);
 }
--- components/_app.tsx
+++ components/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <Component {...pageProps} />
+        
       </NextThemesProvider>
     </NextUIProvider>
   )
