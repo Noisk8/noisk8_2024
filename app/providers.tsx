10e194abd11344fabf401e3202d9bf94e6e6b8ff
noisk8 <juansk8jaramillo@gmail.com>
2023-12-10 04:50:08 -0500

    buke

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,8 +2,19 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import React from 'react';
 
-export function Providers({children}: { children: React.ReactNode }) {
+interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: { // El signo de interrogación indica que la prop es opcional
+    attribute: string;
+    defaultTheme: string;
+  };
+}
+
+export function Providers({children, themeProps}: ProvidersProps) {
+  // Aquí puedes utilizar themeProps como necesites, por ejemplo, para configurar el tema.
+  
   return (
     <NextUIProvider>
       {children}
