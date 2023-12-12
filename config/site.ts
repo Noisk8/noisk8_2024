fc3010e9f47a96c39fbbbf01755779c5f3344dfe
noisk8 <juansk8jaramillo@gmail.com>
2023-12-12 03:06:21 -0500

    change my life

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "ni verga",
+	name: "Noisk8",
+	description: "with love ",
 	navItems: [
 		{
 			label: "Home",
